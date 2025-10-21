a13772bc3f80f8d944e130472c72016fc53074c3
Junichi Eguchi <132245346+JunichiEguchi10010@users.noreply.github.com>
2025-10-21 23:55:12 +0900

    demo-bbs-app

--- sample180/demo-bbs-app/app/page.tsx
+++ sample180/demo-bbs-app/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardAction, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
