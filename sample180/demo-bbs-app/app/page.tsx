2e12ef493c704dfbaa0d7cb419cfedc7e5cf099d
Junichi Eguchi <132245346+JunichiEguchi10010@users.noreply.github.com>
2025-10-25 06:38:07 +0900

    demo-bbs-app

--- sample180/demo-bbs-app/app/page.tsx
+++ sample180/demo-bbs-app/app/page.tsx
@@ -15,27 +15,61 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Card className="w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Login to your account</CardTitle>
-            <CardDescription>
-              Enter your email below to login to your account
-            </CardDescription>
-            <CardAction></CardAction>
-          </CardHeader>
-          <CardContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-            necessitatibus fugit, sit doloribus ad eligendi quod aspernatur!
-            Iusto ipsa necessitatibus, libero explicabo exercitationem minus
-            ratione sunt sequi aut, nihil incidunt?
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <Link href="/bbs-post/1">Read more</Link>
-          </CardFooter>
-        </Card>
-      </main>
-    </div>
+    <main>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <CardAction></CardAction>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+          necessitatibus fugit, sit doloribus ad eligendi quod aspernatur! Iusto
+          ipsa necessitatibus, libero explicabo exercitationem minus ratione
+          sunt sequi aut, nihil incidunt?
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Link href="/bbs-post/1">Read more</Link>
+        </CardFooter>
+      </Card>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <CardAction></CardAction>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+          necessitatibus fugit, sit doloribus ad eligendi quod aspernatur! Iusto
+          ipsa necessitatibus, libero explicabo exercitationem minus ratione
+          sunt sequi aut, nihil incidunt?
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Link href="/bbs-post/1">Read more</Link>
+        </CardFooter>
+      </Card>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <CardAction></CardAction>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+          necessitatibus fugit, sit doloribus ad eligendi quod aspernatur! Iusto
+          ipsa necessitatibus, libero explicabo exercitationem minus ratione
+          sunt sequi aut, nihil incidunt?
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Link href="/bbs-post/1">Read more</Link>
+        </CardFooter>
+      </Card>
+    </main>
   );
 }
